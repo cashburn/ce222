0d67488e3ada4eee57a708bd5e13f54a5ee9d7de
Colin Ashburn <cashburn@purdue.edu>
2016-02-03 20:05:51 -0500

    2016-02-03

---
+++ lab3/forkexec.c
@@ -0,0 +1,19 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <unistd.h>
+
+  int main(int argc, char **argv)
+  {
+    int pid;
+    if (argc==1)
+    {
+      printf("Usage: run <command> [<parameters>]\n"); 
+      exit(1);
+    }
+    if (!(pid = fork())) {
+      execv(argv[1], &argv[1]); 
+      printf("Sorry...   couldn't run that!\n");
+    }
+    printf("Started process %d: %s\n", pid, argv[1]);
+    sleep(30);
+  }
--- lab3/multiforker.c
+++ lab3/multiforker.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
-  FILE * fp = fopen("/ufs/servc03/users/ceashb/Documents/ce222/lab3/multifork-log.txt", "a");
   int pid;
-  fflush(fp);
   for(int i = 0; i < 3; i++){  /* loops 3 times */
     sleep(3);
     fprintf(stdout,"Multiforker PID %d round %d\n", getpid(), i+1);
@@ -14,5 +12,4 @@ int main(int argc, char **argv){
         fprintf(stdout,"inside child %d\n", getpid());
   }
   sleep(30);               /* then sleeps 30 seconds */
-  fclose(fp);
 }
